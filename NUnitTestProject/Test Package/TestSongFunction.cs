493a1a91709c5fbc51c2732426d2aa5fc3d712df
Nguyen Trung Tin <trungtin9620@gmail.com>
2023-07-09 22:24:57 +0700

    update test suite

--- NUnitTestProject/Test Package/TestSongFunction.cs
+++ NUnitTestProject/Test Package/TestSongFunction.cs
@@ -1,10 +1,23 @@
-using NUnitTestForCDStoreManagement.Model;
+﻿using NUnitTestForCDStoreManagement.Model;
 
-namespace NUnitTestingProject.Test_Package 
+namespace NUnitTestingProject.Test_Package
 {
     [TestFixture]
-    public class TestSearchFunction
+    public class TestSongFunction
     {
+        private static IEnumerable<TestCaseData> DurationRegex()
+        {
+            string duration1 = "03:58";
+            string duration2 = "04:55";
+            string duration3 = "-00:12";
+            string duration4 = "55:99";
+            string duration5 = "88:55";
+            yield return new TestCaseData(duration1, duration1 + " is valid");
+            yield return new TestCaseData(duration2, duration2 + " is valid");
+            yield return new TestCaseData(duration3, duration3 + " isn't valid");
+            yield return new TestCaseData(duration4, duration4 + " isn't valid");
+            yield return new TestCaseData(duration5, duration5 + " isn't valid");
+        }
         private static readonly object[] SearchTestCases =
         {
             new object[] {"Greatest", new [] { "The Greatest Hits" }},
@@ -21,6 +34,16 @@ namespace NUnitTestingProject.Test_Package
             new object[] {"awayway", new string[] {}},
             new object[] {"Unknown", new string[] {}}
         };
+
+        [Test]
+        [TestCaseSource(nameof(DurationRegex))]
+        public void CheckLogin_ReturnsExpectedResult(string s, string expectedResult)
+        {
+            var result = SongService.checkRegex(s);
+
+            Assert.AreEqual(expectedResult, result);
+        }
+        
 
 
         [Test, TestCaseSource(nameof(SearchTestCases))]
---
+++ NUnitTestProject/Test Package/TestUser.cs
@@ -0,0 +1,149 @@
+﻿using NUnitTestForCDStoreManagement.Model;
+
+namespace NUnitTestingProject.Test_Package
+{
+    [TestFixture]
+    public class TestUser
+    {
+        private static IEnumerable<TestCaseData> AccountLoginTestCases()
+        {
+            AccountModel acc1 = new AccountModel()
+            {
+                AccountId = 1,
+                UserName = "admin",
+                PassWord = "abc12345",
+                RoleId = "MG",
+                FullName = "Administrator",
+                Email = "cdstore@gmail.com",
+                Address = "FPT University",
+                PhoneNumber = "0886647866",
+            };
+            yield return new TestCaseData(acc1, "Login Successfully!" + "(" + acc1.UserName + ")");
+            AccountModel acc2 = new AccountModel()
+            {
+                AccountId = 2,
+                UserName = "trungtin9620@gmail.com",
+                PassWord = "962000",
+                RoleId = "EM",
+                FullName = "Nguyen Trung Tin",
+                Email = "trungtin9620@gmail.com",
+                Address = "Thu Duc City",
+                PhoneNumber = "0913898913",
+            };
+
+            yield return new TestCaseData(acc2, "Login Successfully!" + "(" + acc2.UserName + ")");
+            AccountModel acc3 = new AccountModel()
+            {
+                AccountId = 3,
+                UserName = "khangmk",
+                PassWord = "khang12345",
+                RoleId = "EM",
+                FullName = "Nguyen Minh Khang",
+                Email = "khangnm11@gmail.com",
+                Address = "Vung Tau",
+                PhoneNumber = "0908256762",
+            };
+            yield return new TestCaseData(acc3, "Login Successfully!" + "(" + acc3.UserName + ")");
+            AccountModel acc4 = new AccountModel()
+            {
+
+            };
+            yield return new TestCaseData(acc4, "Please enter username and password" + "(" + acc4.UserName + ")");
+        }
+        private static IEnumerable<TestCaseData> PhoneNumberTestCases()
+        {
+            string phoneNo1 = "+84123456789";
+            yield return new TestCaseData(phoneNo1, phoneNo1 + " is valid phone number");
+            string phoneNo2 = "084123456789";
+            yield return new TestCaseData(phoneNo2, phoneNo2 + " is not valid phone number");
+            string phoneNo3 = "012345678";
+            yield return new TestCaseData(phoneNo3, phoneNo3 + " is not valid phone number");
+            string phoneNo4 = "0-123456789";
+            yield return new TestCaseData(phoneNo4, phoneNo4 + " is not valid phone number");
+            string phoneNo5 = "0 123 456 789";
+            yield return new TestCaseData(phoneNo5, phoneNo5 + " is not valid phone number");
+        }
+        private static IEnumerable<TestCaseData> UserRoleTestCases()
+        {
+            AccountModel acc1 = new AccountModel()
+            {
+                AccountId = 1,
+                UserName = "admin",
+                PassWord = "abc12345",
+                RoleId = "MG",
+                FullName = "Administrator",
+                Email = "cdstore@gmail.com",
+                Address = "FPT University",
+                PhoneNumber = "0886647866",
+            };
+            yield return new TestCaseData(acc1,"Welcome Admin");
+            AccountModel acc2 = new AccountModel()
+            {
+                AccountId = 2,
+                UserName = "trungtin9620@gmail.com",
+                PassWord = "962000",
+                RoleId = "EM",
+                FullName = "Nguyen Trung Tin",
+                Email = "trungtin9620@gmail.com",
+                Address = "Thu Duc City",
+                PhoneNumber = "0913898913",
+            };
+            yield return new TestCaseData(acc2, "Welcome " + acc2.FullName);
+            AccountModel acc3 = new AccountModel()
+            {
+                AccountId = 3,
+                UserName = "khangmk",
+                PassWord = "khang12345",
+                RoleId = "EM",
+                FullName = "Nguyen Minh Khang",
+                Email = "khangnm11@gmail.com",
+                Address = "Vung Tau",
+                PhoneNumber = "0908256762",
+            };
+            yield return new TestCaseData(acc3, "Welcome " + acc3.FullName);
+        }
+        [Test]
+        [TestCaseSource(nameof(AccountLoginTestCases))]
+        public void CheckLogin_ReturnsExpectedResult(AccountModel account, string expectedResult)
+        {
+            var result = AccountService.Login(account);
+
+            Assert.AreEqual(expectedResult, result);
+        }
+
+        [Test]
+        [TestCaseSource(nameof(UserRoleTestCases))]
+        public void CheckUserRole_ReturnsExpectedResult(AccountModel account, string expectedResult)
+        {
+            // Act
+            var result = AccountService.checkUserRole(account);
+
+            // Assert
+            Assert.AreEqual(expectedResult, result);
+        }
+
+        [TestCase("example@fpt.edu.vn", true)]
+        [TestCase("example.first.middle.lastname@email.com", true)]
+        [TestCase("example@subdomain.email.com", true)]
+        [TestCase("0987654321@example.com", true)]
+        [TestCase("_______@email.com", true)]
+        [TestCase("example.firstname-lastname@email.com", true)]
+        [TestCase("abcde", false)]
+        [TestCase("@#@@##@%^%#$@#$@#.com", false)]
+        [Test]
+        public void CheckEmailRegex(string email, bool expectedResult)
+        {
+            var result = AccountService.isValid(email);
+            Assert.AreEqual(expectedResult, result);
+        }
+
+        [Test]
+        [TestCaseSource(nameof(PhoneNumberTestCases))]
+        public void CheckUserRole_ReturnsExpectedResult(string phoneNumber, string expectedResult)
+        {
+            var result = AccountService.checkPhoneRegex(phoneNumber);
+            Assert.AreEqual(expectedResult, result);
+        }
+
+    }
+}
